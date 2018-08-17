6d1bf4983d4921cf40cb69e476b8a8b7389f2ee1
张铭阳 <mingyoungcheung@gmail.com>
2018-08-17 15:36:59 +0800

    Update Encrypter.php

--- src/Encrypter.php
+++ src/Encrypter.php
@@ -23,13 +23,49 @@ class Encrypter
         $json = json_encode($attributes);
         $key = $this->config[HualalaOptions::AES_KEY];
 
-        // if (extension_loaded('mcrypt')) {
-        //     $encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $json, MCRYPT_MODE_CBC, $key);
-        // } else {
         $encrypted = openssl_encrypt($json, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $key);
-        // }
 
         return base64_encode($encrypted);
+    }
+
+    public function concater($data) {
+        $t = '';
+        foreach ($data as $key => $value) {
+            if (is_bool($value) || is_float($value)) {
+                $value = var_export($value, true);
+            }
+
+            $t .= $key.$value;
+        }
+        return $t;
+    }
+
+    protected function filterAndSort($data, $f= true)
+    {
+        $data = array_filter($data, function ($item) {
+            return ! is_null($item);
+        });
+
+        ksort($data, SORT_STRING | SORT_FLAG_CASE);
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                $data[$key] = $this->filterAndSort($value);
+            }
+        }
+        return $data;
+    }
+
+    protected function resetArray($array)
+    {
+        $reset = [];
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $reset = array_merge($reset, $this->resetArray($value));
+            } else {
+                $reset[$key] = $value;
+            }
+        }
+        return $reset;
     }
 
     /**
@@ -39,28 +75,17 @@ class Encrypter
      */
     public function signature(array $attributes)
     {
-        $separater = function (array $data) {
-            $k = '';
-            foreach ($data as $key => $value) {
-                if (is_bool($value) || is_float($value)) {
-                    $value = var_export($value, true);
-                }
-                $k .= $key.$value;
-            }
+        $maker = function ($data) {
+            $data = $this->resetArray($data);
+            $data = $this->filterAndSort($data);
+            $separated = $this->concater($data);
 
-            return $k;
-        };
-        $maker = function ($data) use ($separater) {
-            $data = array_filter($data);
-            ksort($data);
-            $separated = $separater($data);
             $str = 'key'.$separated.'secret';
-
             return strtoupper(sha1($str));
         };
 
         $base = [
-            'timestamp' => (string) time() * 1000,
+            'timestamp' => (string) (time() * 1000),
             'devID' => (int) $this->config[HualalaOptions::DEVELOPER_ID],
             'merchantsID' => (int) $this->config[HualalaOptions::MERCHANT_ID],
             'groupID' => (int) $this->config[HualalaOptions::GROUP_ID],
